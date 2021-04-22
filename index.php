61554022f3c6a8e2db2474f9e43d8007be592812
Amir morteza Yousefi <amirmy1382@gmail.com>
2021-04-23 00:00:31 +0430

    changing index.php removing tests(echo and var_dump)

--- index.php
+++ index.php
@@ -4,6 +4,3 @@ require __DIR__ . "/‌‌Bootstrap/autoload.php";
 use App\Controller\HomeController;
 
 $homecontroller = new HomeController();
-//echo "{$homecontroller->index()}\n";
-//var_dump($homecontroller);
-echo "ddddd";
