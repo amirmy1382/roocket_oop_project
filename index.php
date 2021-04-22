198597d8904a406ebbf62d8ba6d57e2d26825635
Amir morteza Yousefi <amirmy1382@gmail.com>
2021-04-22 23:55:12 +0430

    composer settings and preparing for autoload -adding autoload (psr-4:App\\ ::> App/) -creating autoload.php for requiring vendor/autoload.php -requiring autoload.php in index.php

--- index.php
+++ index.php
@@ -1,1 +1,9 @@
 <?php
+require __DIR__ . "/‌‌Bootstrap/autoload.php";
+
+use App\Controller\HomeController;
+
+$homecontroller = new HomeController();
+//echo "{$homecontroller->index()}\n";
+//var_dump($homecontroller);
+echo "ddddd";
