bd50ae9dfbc2305f4e1e67cb672f6a57cce7237b
Amir morteza Yousefi <amirmy1382@gmail.com>
2021-04-23 22:34:38 +0430

    chenging names in header.php -title to "Welcome to amirmy's blog" -<a> in line 29 to <href="/">"Amirmy's Blog"</> -tag a in line 36 to href="/"

--- templates/header.php
+++ templates/header.php
@@ -11,7 +11,7 @@ require __DIR__ . "/../‌‌Bootstrap/autoload.php";
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Blog Home - Start Bootstrap Template</title>
+    <title>Blog Home - Welcome to amirmy's blog</title>
 
     <!-- Bootstrap core CSS -->
     <link href="../public/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -26,14 +26,14 @@ require __DIR__ . "/../‌‌Bootstrap/autoload.php";
 <!-- Navigation -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
     <div class="container">
-        <a class="navbar-brand" href="#">Start Bootstrap</a>
+        <a class="navbar-brand" href="/">Amirmy's Blog</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item active">
-                    <a class="nav-link" href="#">Home
+                    <a class="nav-link" href="/">Home
                         <span class="sr-only">(current)</span>
                     </a>
                 </li>
